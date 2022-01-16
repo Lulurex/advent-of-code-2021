0234b5c3d0a6df4d6766343665fb6b7214b3cea6
Lulu h <Lulu@MacBook-Air-3.local>
2022-01-16 02:21:59 -0500

    add many days and folders, finished p1 of day 12

--- day12/day12.py
+++ day12/day12.py
@@ -5,18 +5,24 @@ import numpy as np
 #data = np.genfromtxt('day12test.txt')
 #print(data)
 
-f = open("day12test2.txt", "r")
+f = open("day12test.txt", "r")
 listdata = f.read().splitlines()   # list of strings
 #raw = [y for y in soraw]   # list of ints
 
-print(listdata)
-graph = {
-    "test": "case"
-}
-
-
+graph = {}
 CaveDict = {}
 Verticies = []
+count = 0
+
+def inspectCave():
+    print("which cave do you want to inspect?")
+    whichcave = input()
+
+    c = CaveDict[whichcave]
+    print(c.size)
+    print(c.connectedTo)
+    print("~~~")
+
 
 class Cave:                     # Every cave will be an object
     def __init__(self):
@@ -28,6 +34,9 @@ class Cave:                     # Every cave will be an object
 
     def addConnection(self,newCave):    # How to add a cave
         self.connectedTo.append(newCave)
+
+    def removeConnection(self,removethiscave):
+        self.connectedTo.remove(removethiscave)
 
 # Make each cave into a class, note if it's a big cave and what it's connected to
 for entry in listdata:
@@ -56,41 +65,115 @@ for entry in listdata:
         c.size = "big"
 
 
-
     if a[0] not in Verticies:
         Verticies.append(a[0])      # Append the vertex to the LIST
-        print("adding: " + a[0])
+    if a[1] not in Verticies:
+        Verticies.append(a[1])
 
-    else:
-        print("skip " + a[0])
-
-print("which cave do you want to inspect?")
-whichcave = input()
-
-c = CaveDict[whichcave]
-print(c.size)
-print(c.connectedTo)
-print("~~~")
 #print("all keys")
 #print(CaveDict.keys())
 
+# inspectCave()
 
+for vertex in Verticies:
+    c = CaveDict[vertex]
+    graph[vertex] = c.connectedTo
+
+small_caves = []
+for key in graph.keys():
+    if key.islower() == True:
+        small_caves.append(key)
+print("small caves: " + str(small_caves))
 
 #print(graph)
+paths = []
+starting_cavename = "start"
+
+def find_path(graph, start, end, path = []):
+    path = path + [start]
+    #print("start: " + start)
+    #print("path: " + str(path))
+    if start == end:
+        #print("Destination FOUND")
+        global count
+        count += 1
+        global paths
+        paths.append(path)
+        return path
+    if len(graph[start]) == 1 and start.isupper() == True:
+        #print("DEAD END! going back...")
+        return []
+    helper = graph[start]
+    print(helper)
+    for node in graph[start]:
+        # if        new      and        small          or (     big )
+        if (node not in path and node in small_caves) or (node not in small_caves):
+            #print("node, path")
+            #print(node)
+            #print(path)
+            find_path(graph, node, end, path)
 
 
-#potato = Cave()
-#potato.addConnection("AB")
-#potato.addConnection("AC")
-#print(potato.connectedTo)
-#print(potato.size)
 
-#tomato = Cave()
-#tomato.size = "big"
-#print(tomato.size)
+find_path(graph, starting_cavename, "end")
+print("There are " + str(count) + " caves in this system.")
+print("Here are the paths: ")
+print(paths)
 
 
-#print(Verticies)
-#print(graph)
+''' PART TWO'''
 
-# obtain the connections
+def checkForDoubles(double, path):
+    doubleDict = {}
+    for item in path:
+        if len(doubleDict[itme]) == "2":
+            print("new key")
+            doubleDict[item] = "2"
+        else:
+            print("B")
+
+paths = []
+starting_cavename = "start"
+count = 0
+small_caves = []
+for key in graph.keys():
+    if key == "start":
+        continue
+    if key == "end":
+        continue
+    if key.islower() == True:
+        small_caves.append(key)
+print("small caves: " + str(small_caves))
+
+one_time_cave = ["start", "end"]
+double_cave = ""
+
+def find_path_p2(graph, start, end, path = []):
+    path = path + [start]
+    #print("start: " + start)
+    #print("path: " + str(path))
+    if start == end:
+        print("Destination FOUND")
+        global count
+        count += 1
+        global paths
+        paths.append(path)
+        return path
+    if len(graph[start]) == 1 and start.isupper() == True:
+        print("DEAD END! going back...")
+        return []
+    for node in graph[start]:
+        #               small   and not      start/finish     or    big
+        if (node in small_caves and not one_time_cave) or (node not in small_caves and not small_caves):
+            checkForDoubles(path)
+            #print("node, path")
+            #print(node)
+            #print(path)
+
+            find_path_p2(graph, node, end, path)
+    return None
+
+
+#find_path_p2(graph, starting_cavename, "end")
+print(count)
+print(paths)
